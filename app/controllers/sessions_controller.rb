561791a634435016cca3d92b5b0f465ce43e8f67
nguyen <anh10.14khuatbaonguyen@gmail.com>
2025-07-25 14:57:06 +0700

    Chapter 8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  # GET /login
+  def new; end
+
+  # POST /login
+  def create
+    user = User.find_by email: params.dig(:session, :email)&.downcase
+    if user&.authenticate params.dig(:session, :password)
+      handle_successful_login user
+    else
+      handle_failed_login
+    end
+  end
+
+  # DELETE /logout
+  def destroy
+    log_out
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def handle_successful_login user
+    reset_session
+    log_in user
+    redirect_to user, status: :see_other
+  end
+
+  def handle_failed_login
+    flash.now[:danger] = t(".invalid_email_password_combination")
+    render :new, status: :unprocessable_entity
+  end
+end
