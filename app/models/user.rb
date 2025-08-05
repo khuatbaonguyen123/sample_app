cf5f5da07629efc1d8bdd187dbd7ca5273382717
nguyen <anh10.14khuatbaonguyen@gmail.com>
2025-08-05 16:02:12 +0700

    Chapter 14

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,19 @@ class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
 
+  has_many :active_relationships,
+           class_name: Relationship.name,
+           foreign_key: :follower_id,
+           dependent: :destroy
+
+  has_many :passive_relationships,
+           class_name: Relationship.name,
+           foreign_key: :followed_id,
+           dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   class << self
     def digest string
       cost = if ActiveModel::SecurePassword.min_cost
@@ -100,7 +113,26 @@ class User < ApplicationRecord
   end
 
   def feed
-    microposts.order(created_at: :desc)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+             .includes(:user, image_attachment: :blob)
+  end
+
+  # Follows a user
+  def follow other_user
+    following << other_user
+  end
+
+  # Unfollows a user
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  # Returns true if the current user is following the other_user
+  def following? other_user
+    following.include? other_user
   end
 
   private
